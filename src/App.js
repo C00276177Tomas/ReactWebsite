7698a69146333f9b9bf4eb64f8125963333dfc54
Tomas Smitas <C00276177@setu.ie>
2024-08-27 15:20:15 +0100

    Experience, Underlining, Footer added

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
             <a href="https://github.com/C00276177Tomas" target="_blank" rel="noopener noreferrer">
               <img src={githubLogo} alt="GitHub" className="social-logo" />
             </a>
-            <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/tomas-smitas-a43aa2104/" target="_blank" rel="noopener noreferrer">
               <img src={linkedInLogo} alt="GitHub" className="social-logo" />
             </a>
           </div>
@@ -63,30 +63,45 @@ function App() {
       <main>
         <section id="intro">
           <h1>My vision and passion</h1>
-          <p>For me, the most important aspect of software is functionality. I appreciate simplicity and clarity above all.
-            I enjoy frontend and backend development. I have a passionate interest in AI, my main iterest includes the implementation
-            of AI to solve real world problems.
+          <p>
+            For me, the core of software development is functionality. I value simplicity and clarity, focusing on creating both 
+            intuitive frontends and efficient backends. My goal is to develop solutions that effectively address real-world problems 
+            and enhance user experiences.
+          </p>
+
+          <p>
+            I am particularly passionate about artificial intelligence and its potential to drive innovation. I am dedicated to 
+            applying AI to tackle complex challenges and improve technological solutions. By blending my technical skills with a 
+            strong interest in AI, I aim to contribute to meaningful advancements and create impactful, practical applications.
           </p>
         </section>
         <section id="experience">
-          <h2>About Us</h2>
-          <p>Learn more about what we do.</p>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
+          <h2>Experience</h2>
+          <div class="job">
+              <h3>Netwatch – Research & Development Department (Software Development Intern)</h3>
+              <p class="dates">Jan 2024 – July 2024</p>
+              <ul>
+                  <li>Contributed to the development of software solutions across web pages, mobile apps (Android and iOS), and in-house applications within a Scrum framework.</li>
+                  <li>Performed coding, debugging, and testing applications, collaborating closely with senior developers.</li>
+                  <li>Gained extensive knowledge in areas such as Azure DevOps, Git, Visual Studio, Visual Studio Code, React, Typescript, C#, Team Coding, GraphQL, SQL, Packages, Libraries, Test Cases, Mobile Applications, etc.</li>
+                  <li>Implemented new features and resolved bugs and defects in existing software, improving user experience.</li>
+                  <li>Participated in team meetings like Sprint Planning, Daily Stand Up, Weekly Stand Up, Planning Poker, Peer Reviews, Team Days Out, etc.</li>
+                  <li>Contributed to documentation and user manuals, aiding in the understanding of React's Material React Tables V2 for developers that haven’t worked on MRT before.</li>
+              </ul>
+          </div>
+
+          <div class="job">
+              <h3>Integer – Medical Manufacturing (General Operative)</h3>
+              <p class="dates">Sept 2018 – Jan 2024</p>
+              <ul>
+                  <li>Worked in multiple departments on assembly lines with team sizes of 4 to 8 to produce life-saving equipment that was subject to very high-quality standards.</li>
+                  <li>Worked on the module which is a big machine that needs to be constantly maintained. My tasks included cutting coils to length, loading machine, maintaining loaders, maintaining welders, lasering, preventative maintenance, inspecting welds, and reworking or disposing of defective guides.</li>
+                  <li>Operated and maintained precision manufacturing equipment, ensuring adherence to strict quality standards.</li>
+                  <li>Collaborated with cross-functional teams to meet production targets, contributing to the timely delivery of medical devices.</li>
+                  <li>Conducted quality inspections and tests, ensuring products met regulatory compliance and specifications.</li>
+                  <li>Worked with a kanban system that ensured efficiency.</li>
+              </ul>
+          </div>
         </section>
         <section id="projects">
           <h2>Our Services</h2>
@@ -108,11 +123,22 @@ function App() {
           <br></br>
           <br></br>
         </section>
-        <section id="contact">
-          <h2>Contact Us</h2>
-          <p>Get in touch with us.</p>
-        </section>
       </main>
+      <footer>
+        <section id="contact">
+        <p>Contact Information:</p>
+        <p>Email: <a href="mailto:tomassmitas@yahoo.com">tomassmitas@yahoo.com</a></p>
+        <p>Phone: 083 358 8363</p>
+        <div className="social-media-linear">
+          <a href="https://github.com/C00276177Tomas" target="_blank" rel="noopener noreferrer">
+            <img src={githubLogo} alt="GitHub" className="social-logo-linear" />
+          </a>
+          <a href="https://www.linkedin.com/in/tomas-smitas-a43aa2104/" target="_blank" rel="noopener noreferrer">
+            <img src={linkedInLogo} alt="GitHub" className="social-logo-linear" />
+          </a>
+        </div>
+        </section>
+      </footer>
     </div>
   );
 }
